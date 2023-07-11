771b1e0a012c0890119faac572fabc80dc694f33
MD BIPLOB HOSSAIN <108410378+Biplob17h@users.noreply.github.com>
2023-07-11 10:52:10 -0600

    frank comit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ function App() {
   const rise = () =>{
     setNumber(number + 1)
   }
+  const decress = () =>{
+    setNumber(number - 1)
+  }
   return (
     <div className="App">
       <h1>wellcome</h1>
       <button onClick={rise}> + </button>
       <h1>{number}</h1>
+      <button onClick={decress}>-</button>
     </div>
 
 
